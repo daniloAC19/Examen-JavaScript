298da926ec2b15435e6c9be14a8fbe84812b70bb
DaniloAC <daniloac95@gmail.com>
2018-11-30 17:56:35 -0500

    Pregunta 2 y 3

---
+++ OneDrive_2018-11-30/2018-b-gr1-master/examen-01/pokemon/Examen1.js
@@ -0,0 +1,19 @@
+
+//Pregunta 2
+const arregloRespuestaTypes = data.json().find(function (pokemon){
+    return pokemon =  'abilities';
+});
+
+console.log(arregloRespuestaTypes);
+
+
+//Pregunta 3
+const arregloRespuestaMoves = data.json().find(function (element){
+    return element =  'move';
+});
+
+console.log(arregloRespuestaMoves);
+
+//Pregunta 4
+
+
